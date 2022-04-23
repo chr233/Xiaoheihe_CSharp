321895de78c65c11c59c0c1bf33c4bebf48eec35
Chr_ <chr@chrxw.com>
2022-04-23 22:35:17 +0800

    添加编译脚本

---
+++ Xiaoheihe_CShape/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: System.CLSCompliant(false)]
+[assembly: AssemblyVersion("1.0.0.002")]
+[assembly: AssemblyFileVersion("1.0.0.002")]
+
+[assembly: AssemblyCopyright("Copyright © 2022 Chr_")]
+[assembly: AssemblyProduct("ASFEnhance")]
+[assembly: AssemblyCompany("chrxw.com")]
+[assembly: AssemblyTitle("An ArchiSteamFarm plugin developed by Chr_.")]
+[assembly: AssemblyDescription("An ArchiSteamFarm plugin developed by Chr_.")]
